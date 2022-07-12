e8184c0dfd1046404e085489f3f2e041bfe01224
adrianaVlongo <66226590+AdrianaViabL@users.noreply.github.com>
2022-07-12 20:02:01 -0300

    new info

--- pooPythonAlura/conta.py
+++ pooPythonAlura/conta.py
@@ -1,3 +1,21 @@
-
 class Conta:
-    pass
+
+    def __init__(self, numero, titular, saldo, limite):
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print(f'Saldo de {self.__saldo} do titular {self.__titular}')
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def transferir(self, valor, destino):  # deixando de forma implicita que a conta de origem é
+        # a ja instanciada dentro do self
+        self.sacar(valor=valor)
+        destino.depositar(valor=valor)
