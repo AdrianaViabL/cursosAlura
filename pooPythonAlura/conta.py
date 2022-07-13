87fdc0f323dccd543d1d53e1a3a2c6dda655572a
adrianaVlongo <66226590+AdrianaViabL@users.noreply.github.com>
2022-07-12 21:04:52 -0300

    new info

--- pooPythonAlura/conta.py
+++ pooPythonAlura/conta.py
@@ -9,8 +9,14 @@ class Conta:
     def extrato(self):
         print(f'Saldo de {self.__saldo} do titular {self.__titular}')
 
+    def __pode_sacar(self, valor):
+        return valor <= (self.__saldo + self.__limite)
+
     def sacar(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor=valor):
+            self.__saldo -= valor
+            return 'saque efetuado'
+        return 'saldo insuficiente para saque'
 
     def depositar(self, valor):
         self.__saldo += valor
@@ -19,3 +25,18 @@ class Conta:
         # a ja instanciada dentro do self
         self.sacar(valor=valor)
         destino.depositar(valor=valor)
+
+    def get_saldo(self): # forma mais simples de acessar os dados de uma variavel encapsulada
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property # transformando esse metodo em uma propriedade da variavel limite, podendo ser acessado sem precisar do
+    # () no final
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, valor):
+        self.__limite = valor
