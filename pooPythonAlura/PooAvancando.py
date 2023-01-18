b1a260d80425b443203548a284bbeb9d16c6a474
adrianaVlongo <66226590+AdrianaViabL@users.noreply.github.com>
2023-01-18 15:56:28 -0300

    herança feita :)

--- pooPythonAlura/PooAvancando.py
+++ pooPythonAlura/PooAvancando.py
@@ -1,46 +1,36 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter  # definindo como os valores serao atribuidos a variavel nome
+    def nome(self, new_nome):
+        self._nome = new_nome.title()
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
 
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter # definindo como os valores serao atribuidos a variavel nome
-    def nome(self, new_nome):
-        self.__nome = new_nome.title()
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter # definindo como os valores serao atribuidos a variavel nome
-    def nome(self, new_nome):
-        self.__nome = new_nome.title()
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
